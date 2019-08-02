06709a4355a15c2401009753e6031de2ea12fa98
IradNuriel <53393842+IradNuriel@users.noreply.github.com>
2019-08-03 00:15:04 +0300

    added the ability to see the build of the heap

--- sketch.js
+++ sketch.js
@@ -5,17 +5,18 @@ let finished = false;
 let colors = [];
 let numSwaps = 0;
 let p;
-
+let s;
 function setup() {
     createCanvas(1200, 600);
     p = createP("0");
-    i = 600;
+    i = 2000;
     for (let j = 0; j <= ceil(Math.log2(i)); j++) {
         colors[j] = { r: random(0, 255), g: random(0, 200), b: random(0, 255) };
     }
-    for (let j = 0; j <= i; j++) {
+    for (let j = 0; j < i; j++) {
         items.push(floor((height - 8) * random()));
     }
+    s = floor((items.length / 2));
 }
 
 function swap(i, j) {
@@ -24,30 +25,24 @@ function swap(i, j) {
     items[j] = temp;
     numSwaps++;
 }
-
-async function makeHeap() {
-    if (!init) {
-        for (let j = floor((items.length / 2)); j >= 0; j--) {
-            await maxHeapify(j);
-        }
+async function makeHeap(j) {
+    if (j >= 0) {
+        maxHeapify(j);
+        s--;
+    } else {
         init = true;
         i = items.length - 1;
     }
 }
-
-
-
 async function sortHeap(size) {
     swap(size, 0);
-    await maxHeapify(0);
+    maxHeapify(0);
     i--;
 }
-
-
-async function maxHeapify(index) {
+function maxHeapify(index) {
     const l = index * 2;
     const r = index * 2 + 1;
-    let largest;
+    let largest = -1;
     if ((l < i) && (items[l] > items[index])) {
         largest = l;
     } else {
@@ -58,35 +53,35 @@ async function maxHeapify(index) {
     }
     if (largest != index) {
         swap(largest, index);
-        await maxHeapify(largest);
+        maxHeapify(largest);
     }
 }
-
 async function draw() {
-    if (!finished) {
-        background(0);
-        await makeHeap();
+    background(0);
+    if (!init) {
+        makeHeap(s);
+    } else if (!finished && init) {
         await sortHeap(i);
-        let xresolution = (width / items.length);
-        for (let j = 0; j < items.length; j++) {
-            const item = items[j];
-            if (j == 0) {
-                const col = colors[0];
-                fill(col.r, col.g, col.b);
-            } else if (j < i) {
-                const level = floor(Math.log2(j) + 1);
-                const col = colors[level];
-                fill(col.r, col.g, col.b);
-            } else {
-                fill(0, 255, 0);
-            }
-            stroke(255);
-            strokeWeight(0.01);
-            rect((j * xresolution), height, xresolution, -1 * item);
-        }
     }
     if (i < 0) {
         finished = true;
+    }
+    let xresolution = (width / (items.length));
+    for (let j = 0; j < items.length; j++) {
+        const item = items[j];
+        if (j == 0) {
+            const col = colors[0];
+            fill(col.r, col.g, col.b);
+        } else if (j < i) {
+            const level = floor(Math.log2(j) + 1);
+            const col = colors[level];
+            fill(col.r, col.g, col.b);
+        } else {
+            fill(0, 255, 0);
+        }
+        stroke(255);
+        strokeWeight(0.01);
+        rect((j * xresolution), height, xresolution, -1 * item);
     }
     p.remove();
     p = createP("number of swaps is: " + numSwaps);
